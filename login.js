6174a8887e4168273dc50be5f74748d363679dfc
VanshMittal109 <vanshcodes15774@gmail.com>
2024-12-31 20:40:52 +0530

    login page

---
+++ login.js
@@ -0,0 +1,22 @@
+const wrapper = document.querySelector(".wrapper")
+const LoginLink = document.querySelector(".login-link")
+const RegisterLink = document.querySelector(".register-link")
+const btnPopup = document.querySelector(".btnLogin")
+const iconClose = document.querySelector(".icon-close")
+
+RegisterLink.addEventListener("click", ()=> {
+    wrapper.classList.add("active");
+})
+
+LoginLink.addEventListener("click", ()=> {
+    wrapper.classList.remove("active");
+})
+
+btnPopup.addEventListener("click", ()=> {
+    wrapper.classList.add("active-popup");
+})
+
+iconClose.addEventListener("click", ()=> {
+    wrapper.classList.remove("active-popup");
+})
+
